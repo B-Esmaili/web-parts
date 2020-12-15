7515c30b4f2070049968cfb5dd24cec1ffff9793
b.esmaili <beehnam.esmaili@gmail.com>
2020-12-15 21:28:04 +0330

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+        extend: {
+            ringWidth: {
+                'DEFAULT': '2px',
+                '6': '6px',
+                '10': '10px',
+            }
+        }
+    }
+}
